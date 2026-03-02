3678d1ebf326955a8155c4d76f2e8668691dc5d7
roshan.khandagale07@gmail.com <roshan.khandagale07@gmail.com>
2026-03-02 18:55:08 +0530

    CONSTANTS added

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,4 +1,11 @@
 MESSAGES = {
     # ROOT MESSAGES
     "API_RUNNING": "ERP System API is running!",
+
+    # AUTH MESSAGES
+    "USER_CREATED": "User created successfully.",
+    "USER_ALREADY_EXISTS": "User with this email already exists.",
+    "USER_NOT_FOUND": "User not found.",
+    "INVALID_CREDENTIALS": "Invalid email or password.",
+    
 }
